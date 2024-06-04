2c614d8ee965662e056979d7f2095481fa4e6757
SisterHailey <153870970+SisterHailey@users.noreply.github.com>
2024-06-03 21:49:11 -0400

    Error hint, renamed file

--- start.bat
+++ start.bat
@@ -1,3 +1,8 @@
 @echo off
 start "" /B "pythonw.exe" "smpwd.pyw"
+if %errorlevel% neq 0 (
+    echo An error occurred. The script did not execute successfully.
+    echo Do you have Python installed?
+    pause
+)
 exit
